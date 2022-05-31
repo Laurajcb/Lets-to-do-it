911316f20c394036da05bd8e244fa53e3278d31d
Laurajcb <callejaslaura96@gmail.com>
2022-05-31 11:10:29 -0500

    chenge distrubucion on files

---
+++ first-react/src/App/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { AppUi } from "./AppUi";
+
+const defaultTodos = [
+  { id: 1, text: "Cut onions", completed: false },
+  { id: 2, text: "Take react course", completed: false },
+  { id: 3, text: "Cry ", completed: false },
+  { id: 4, text: "Lalalal ", completed: true },
+  { id: 5, text: "Nononono ", completed: false },
+];
+
+function App() {
+  const [todos, setTodos] = React.useState(defaultTodos);
+  const [searchValue, setSearchValue] = React.useState("");
+
+  const completedTodos = todos.filter((todo) => !!todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter((todo) => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    });
+  }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+
+    newTodos[todoIndex] = {
+      text: todos[todoIndex].text,
+      completed: true,
+    };
+    setTodos(newTodos);
+  };
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
+
+  return (
+    <AppUi
+      totalTodos={totalTodos}
+      completedTodos={completedTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
+  );
+}
+
+export default App;
--- first-react/src/TodoItem/TodoItem.js
+++ first-react/src/TodoItem/TodoItem.js
@@ -2,24 +2,19 @@ import React from "react";
 import "./TodoItem.css";
 
 function TodoItem(props) {
-  const onComplete = () => {
-    alert("Well done " + props.text);
-  };
-  const onDelete = () => {
-    alert("You delete a to do");
-  };
+
   return (
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
-        onClick={onComplete}
+        onClick={props.onComplete}
       >
         √
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete" onClick={onDelete}>
+      <span className="Icon Icon-delete" onClick={props.onDelete}>
         X
       </span>
     </li>
