d48c8da85bd41308640eb53b8079a744ffcd92cb
Laurajcb <callejaslaura96@gmail.com>
2022-06-29 10:50:59 -0500

    Done some improvements on todoBadges and styles

--- first-react/src/TodoBadges/index.js
+++ first-react/src/TodoBadges/index.js
@@ -1,32 +1,38 @@
 import React from "react";
+import { TodoContext } from "../TodoContext";
 import "./styles.css"
 
 function TodoBadges() {
-
+    const { completedTodos } = React.useContext(TodoContext);
     const badges = [
-        { id: 'one', image: 'one.png', label: 'Great Start!!', achived: true },
-        { id: 'two', image: 'two.png', label: 'You are doing well!!', achived: false },
-        { id: 'three', image:'three.jpg', label:'Keep going!!', achived: false},
-        { id: 'four', image:'four.png', label:'!!', achived: false},
-        { id: 'five', image:'five.png', label:'Keep going!!', achived: false},
-        { id: 'six', image:'six.png', label:'Keep going!!', achived: false},
-        { id: 'seven', image:'seven.png', label:'Keep going!!', achived: false},
-        { id: 'eigth', image:'eigth.png', label:'Keep going!!', achived: false},
-        { id: 'nine', image:'nine.png', label:'Keep going!!', achived: false},
+        { id: 'one', image: 'one.png', goal: 1 },
+        { id: 'two', image: 'two.png', goal: 3 },
+        { id: 'three', image: 'three.jpg', goal: 5 },
+        { id: 'four', image: 'four.png', goal: 10 },
+        { id: 'five', image: 'five.png', goal: 11 },
+        { id: 'six', image: 'six.png', goal: 15 },
+        { id: 'seven', image: 'seven.png', goal: 18 },
+        { id: 'eigth', image: 'eigth.png', goal: 20 },
+        { id: 'nine', image: 'nine.png', goal: 21 },
     ];
-    
+
 
     return (
-        <div className="badges-main-section">
-            {badges.map((badge) => (
-                <section key={badge.id} className="badge-box">
-                    <section className="badges-section" >
-                        <img className="badge-img" src={`../assets/img/${badge.image}`} />
-                        <p>{badge.label}</p>
-                    </section>
-                </section>
-            ))}
-        </div>
+        <section>
+            <div className="container">
+                <h3 className="badges-tittle">Your badges</h3>
+                <div className="badges-main-section">
+                    {badges.map((badge) => (
+                        <section key={badge.id} className={`badge-box ${(completedTodos < badge.goal) ? 'disabled' : ''}`}>
+                            <section className="badges-section">
+                                <div className="overlay"></div>
+                                <img className="badge-img" src={`../assets/img/${badge.image}`} />
+                            </section>
+                        </section>
+                    ))}
+                </div>
+            </div>
+        </section>
     );
 }
 
